c65351e36e2911cdf593045929ee13577faab473
Franciszek Szaniecki <franekszaniecki@gmail.com>
2023-02-16 23:40:46 +0100

    Create basic class for User.

--- src/main/java/com/dev/budgetmanager/conroller/UserController.java
+++ src/main/java/com/dev/budgetmanager/conroller/UserController.java
@@ -1,7 +1,30 @@
 package com.dev.budgetmanager.conroller;
 
-import org.springframework.stereotype.Controller;
+import com.dev.budgetmanager.model.User;
+import com.dev.budgetmanager.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>>getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User>getUserById(@PathVariable("id") Long userId){
+        return new ResponseEntity<>(userService.getUserById(userId),HttpStatus.OK);
+    }
+
 }
---
+++ src/main/java/com/dev/budgetmanager/service/UserService.java
@@ -0,0 +1,32 @@
+package com.dev.budgetmanager.service;
+
+import com.dev.budgetmanager.exception.UserNotFoundException;
+import com.dev.budgetmanager.model.User;
+import com.dev.budgetmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    private UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long userId){
+        return userRepository.findUserById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User by id: "+userId+" wasn't found in data base." ));
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
